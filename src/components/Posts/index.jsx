e715e6a6e53958d497746440105ada663d6ce6e8
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-06 16:53:38 +0700

    functionality interaction

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { useState } from "react";
 import "./Posts.scss";
 
 // Icons
@@ -8,6 +8,18 @@ import ReplyIcon from "@mui/icons-material/Reply";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 
 const Posts = ({ post }) => {
+    const { username, date, title, tag, view, countComment, countShare } = post;
+
+    const [countfavourite, setCountFavourite] = useState(post.countfavourite);
+    const [isFavourite, setIsFavourite] = useState(false);
+
+    const handleFavourite = () => {
+        setCountFavourite(
+            isFavourite ? countfavourite - 1 : countfavourite + 1
+        );
+        setIsFavourite(!isFavourite);
+    };
+
     return (
         <div className="hvdPosts">
             <div className="postWrapper">
@@ -23,50 +35,90 @@ const Posts = ({ post }) => {
                                     flexDirection: "column",
                                 }}
                             >
-                                <div className="post__username">
-                                    {post.username}
-                                </div>
-                                <div className="post__date">{post.date}</div>
+                                {/* username */}
+                                <div className="post__username">{username}</div>
+                                {/* date */}
+                                <div className="post__date">{date}</div>
                             </div>
                         </div>
                         <div className="post__following">Theo Dõi</div>
                     </div>
 
                     <div className="post__body">
-                        <div className="post__title">{post.title}</div>
+                        {/* title */}
+                        <div className="post__title">{title}</div>
                         <div className="post__content">
                             <div>{post.desc}</div>
                             <div>
                                 <img src={post.photo} alt="" />
                             </div>
                         </div>
-                        <div className="post__topic">{post.tag}</div>
+                        {/* tag */}
+                        <div className="post__topic">{tag}</div>
                     </div>
 
                     <div className="post__footer">
-                        <div className="like">
-                            <i className="favoriteIcon">
-                                <FavoriteIcon />
-                            </i>
-                            <span>Yêu thích</span>
+                        <div className="showInteractive">
+                            <div className="like">
+                                <i className="favoriteIcon">
+                                    <FavoriteIcon />
+                                </i>
+                                <span>
+                                    {countfavourite === 0
+                                        ? ""
+                                        : countfavourite + " yêu thích"}
+                                </span>
+                            </div>
+
+                            <div className="comment">
+                                <i className="commentIcon">
+                                    <ChatBubbleOutlineIcon />
+                                </i>
+                                <span>
+                                    {countComment === 0
+                                        ? ""
+                                        : countComment + " bình luận"}
+                                </span>
+                            </div>
+
+                            <div className="share">
+                                <i className="shareIcon">
+                                    <ReplyIcon />
+                                </i>
+                                <span>
+                                    {countShare === 0
+                                        ? ""
+                                        : countShare + " chia sẻ"}
+                                </span>
+                            </div>
+
+                            <div className="view">
+                                <i className="viewIcon">
+                                    <VisibilityIcon />
+                                </i>
+                                <span>{view}</span>
+                            </div>
                         </div>
-                        <div className="comment">
-                            <i className="commentIcon">
-                                <ChatBubbleOutlineIcon />
-                            </i>
-                            <span>Bình luận</span>
-                        </div>
-                        <div className="share">
-                            <i className="shareIcon">
-                                <ReplyIcon />
-                            </i>
-                            <span>Chia sẻ</span>
-                        </div>
-                        <div className="view">
-                            <i className="viewIcon">
-                                <VisibilityIcon />
-                            </i>
-                            <span>View</span>
+
+                        <div className="postInteraction">
+                            <div className="like" onClick={handleFavourite}>
+                                <i className="favoriteIcon">
+                                    <FavoriteIcon />
+                                </i>
+                                <span>Yêu thích</span>
+                            </div>
+                            <div className="comment">
+                                <i className="commentIcon">
+                                    <ChatBubbleOutlineIcon />
+                                </i>
+                                <span>Bình luận</span>
+                            </div>
+                            <div className="share">
+                                <i className="shareIcon">
+                                    <ReplyIcon />
+                                </i>
+                                <span>Chia sẻ</span>
+                            </div>
                         </div>
                     </div>
                 </div>
