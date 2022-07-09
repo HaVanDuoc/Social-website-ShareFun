32c43b0116dce4562d8e5ac5eb0a8b55d4c84311
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-09 15:29:03 +0700

    optimize componet avatar

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import "./Avatar.scss";
 
 const Avatar = (props) => {
-    const { children, ...otherProps } = props;
+    const { src, children, ...otherProps } = props;
+
+    const handleClick = () => {
+        window.location = "http://localhost:3000/profile";
+    };
+
     return (
-        <div className="hvdAvatar" style={{ ...otherProps }}>
-            <div className="avatarWrapper">{children}</div>
+        <div
+            className="hvdAvatar"
+            style={{ ...otherProps }}
+            onClick={handleClick}
+        >
+            <div className="avatarWrapper">
+                <img src={src} alt="avatar" />
+            </div>
         </div>
     );
 };
