a622a757d6add318f9ba86cca56f6aacbcd86033
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-26 21:35:44 +0700

    [SERVER] delete user

--- server/routes/users.js
+++ server/routes/users.js
@@ -10,7 +10,7 @@ router.get("/", async (req, res) => {
 
 // update user
 router.put("/:id", async (req, res) => {
-    if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
         if (req.body.password) {
             try {
                 const salt = await bcrypt.genSalt(10);
@@ -29,6 +29,20 @@ router.put("/:id", async (req, res) => {
         }
     } else {
         return res.status(403).json("You can update only your account");
+    }
+});
+
+// delete user
+router.delete("/:id", async (req, res) => {
+    if (req.body.userId === req.params.id || req.body.isAdmin) {
+        try {
+            const user = await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("Account has been deleted");
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    } else {
+        return res.status(403).json("You can delete only your account");
     }
 });
 
