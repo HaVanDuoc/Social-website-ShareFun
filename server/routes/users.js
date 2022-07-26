dfda602a7d1621e34b43ab95b6a6bb301ed08fc2
Hà Văn Được <havanduoc.dev@gmail.com>
2022-07-26 21:50:15 +0700

    SERVER get a user

--- server/routes/users.js
+++ server/routes/users.js
@@ -46,4 +46,15 @@ router.delete("/:id", async (req, res) => {
     }
 });
 
+// get a user
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, updateAt, ...other } = user._doc;
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 module.exports = router;
