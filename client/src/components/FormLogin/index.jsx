1aa02b37ec66503c8df29a4457b9af2538bb3b57
Hà Văn Được <havanduoc.dev@gmail.com>
2022-08-26 15:29:47 +0700

    component FormLogin

--- client/src/components/FormLogin/index.jsx
+++ client/src/components/FormLogin/index.jsx
@@ -1,67 +1,28 @@
-import React, { useState } from "react";
-import FieldInput from "./FieldInput";
-import "./FormLogin.scss";
+import React, { useRef } from 'react'
+import './FormLogin.scss'
 
-const FormLogin = () => {
-    const [values, setValues] = useState({
-        username: "",
-        password: "",
-    });
-
-    const ArrayInputs = [
-        {
-            id: 1,
-            name: "username",
-            type: "text",
-            placeholder: "Email hoặc số điện thoại",
-            errorMessage:
-                "Email hoặc số di động bạn nhập không kết nối với tài khoản nào.",
-            require: "true",
-        },
-        {
-            id: 2,
-            name: "password",
-            type: "password",
-            placeholder: "Mật khẩu",
-            require: "true",
-        },
-    ];
+function FormLogin() {
+    const email = useRef()
+    const password = useRef();
 
     const handleSubmit = (e) => {
         e.preventDefault();
     };
-
-    const onChange = (e) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
-    };
-
     return (
         <form
             action=""
-            id="formLogin"
             className="formLogin"
             onSubmit={handleSubmit}
         >
             <div className="title">Đăng nhập HaVanDuoc</div>
-
-            {ArrayInputs.map((input) => (
-                <FieldInput
-                    key={input.id}
-                    {...input}
-                    value={values[input.name]}
-                    onChange={onChange}
-                />
-            ))}
-
-            <button type="submit" className="button loginBtn">
-                Đăng nhập
-            </button>
-
+            <input type="email" placeholder="Email" required ref={email} />
+            <input type="password" placeholder="Password" required minLength={6} ref={password} />
+            <button type="submit" className="button loginBtn">Đăng nhập</button>
             <div className="forgotPasswordLink">Quên mật khẩu?</div>
             <hr />
             <button className="button registerBtn">Tạo tài khoản mới</button>
         </form>
-    );
-};
+    )
+}
 
-export default FormLogin;
+export default FormLogin
