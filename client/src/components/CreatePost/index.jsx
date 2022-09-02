6fcafe3547e910894d958f9f0a41e67df0cb6a1a
Hà Văn Được <havanduoc.dev@gmail.com>
2022-09-02 22:00:55 +0700

    Update components CreatePost

--- client/src/components/CreatePost/index.jsx
+++ client/src/components/CreatePost/index.jsx
@@ -1,21 +1,25 @@
 import React from "react";
 import "./CreatePost.scss";
 
-import { Avatar } from "../../components";
-
 import VideoCameraFrontIcon from "@mui/icons-material/VideoCameraFront";
 import PhotoIcon from "@mui/icons-material/Photo";
 import MoodIcon from "@mui/icons-material/Mood";
+import { Avatar, Box } from "@mui/material";
+import { useContext } from "react";
+import AuthContext from "../../store/AuthContext";
 
 const CreatePost = () => {
+    const [state] = useContext(AuthContext)
+    const { user } = state
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER
+
     return (
         <div className="hvdCreatePost">
             <div className="createPostWrapper">
                 <div className="childTop">
-                    <Avatar
-                        src="/images/avatar/avt.jpg"
-                        border="1px solid lightgray"
-                    />
+                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center', cursor: 'pointer' }}>
+                        <Avatar src={user ? user.avatar : `${PF}images/noUser.png`} sx={{ width: 40, height: 40 }} />
+                    </Box>
                     <input type="text" placeholder="Bạn đang nghĩ gì thế?" />
                 </div>
                 <div className="childBottom">
