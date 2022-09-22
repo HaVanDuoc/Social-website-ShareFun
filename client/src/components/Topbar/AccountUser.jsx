80d611adada7169a49bf2f9fb6991b5f75679c15
Hà Văn Được <havanduoc.dev@gmail.com>
2022-09-22 23:18:41 +0700

    UPDATE - component AccountUser

--- client/src/components/Topbar/AccountUser.jsx
+++ client/src/components/Topbar/AccountUser.jsx
@@ -13,143 +13,151 @@ import Logout from '@mui/icons-material/Logout';
 import { useDispatch } from 'react-redux';
 import { OpenModalLogin } from '../../redux/actions/ModalAction';
 import { FormLogin } from '../../redux/actions/SignInOutAction';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AccountUser = () => {
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-    const isLoggedIn = localStorage.getItem('isLoggedIn');
-
     const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isLoggedIn = localStorage.getItem('isLoggedIn');
+    const [user, setUser] = useState({});
 
-    const handleClickLoggedIn = (e) => {
-        e.preventDefault();
-        
-        setAnchorEl(e.currentTarget);
-    };
-
-    const handleClickNoLoggedIn = (e) => {
-        e.preventDefault();
-
-        if (isLoggedIn === null) {
-            dispatch(FormLogin());
-            dispatch(OpenModalLogin());
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem('user');
+        if (loggedInUser) {
+            const foundUser = JSON.parse(loggedInUser);
+            setUser(foundUser);
         }
-    };
+    }, []);
 
-    const handleClickLogOut = (e) => {
-        e.preventDefault();
+    const Logged = () => {
+        const [anchorEl, setAnchorEl] = React.useState(null);
+        const open = Boolean(anchorEl);
 
-        localStorage.removeItem('user');
-        localStorage.removeItem('isLoggedIn');
-    };
+        const handleClickLoggedIn = (e) => {
+            e.preventDefault();
+            setAnchorEl(e.currentTarget);
+        };
 
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
+        const handleClickLogOut = (e) => {
+            e.preventDefault();
+            localStorage.removeItem('user');
+            localStorage.removeItem('isLoggedIn');
+        };
 
-    return (
-        <Fragment>
-            {isLoggedIn ? (
-                // Logged
-                <Fragment>
-                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                        <Tooltip title="Đăng nhập">
-                            <IconButton
-                                onClick={handleClickLoggedIn}
-                                size="small"
-                                sx={{ ml: 2 }}
-                                aria-controls={open ? 'account-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={open ? 'true' : undefined}
-                            >
-                                <Avatar sx={{ width: 40, height: 40 }}></Avatar>
-                            </IconButton>
-                        </Tooltip>
-                    </Box>
-                    <Menu
-                        anchorEl={anchorEl}
-                        id="account-menu"
-                        open={open}
-                        onClose={handleClose}
-                        onClick={handleClose}
-                        PaperProps={{
-                            elevation: 0,
-                            sx: {
-                                overflow: 'visible',
-                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                                mt: 1.5,
-                                '& .MuiAvatar-root': {
-                                    width: 32,
-                                    height: 32,
-                                    ml: -0.5,
-                                    mr: 1,
-                                },
-                                '&:before': {
-                                    content: '""',
-                                    display: 'block',
-                                    position: 'absolute',
-                                    top: 0,
-                                    right: 14,
-                                    width: 10,
-                                    height: 10,
-                                    bgcolor: 'background.paper',
-                                    transform: 'translateY(-50%) rotate(45deg)',
-                                    zIndex: 0,
-                                },
+        const handleClose = () => {
+            setAnchorEl(null);
+        };
+
+        const handleClickProfile = (e) => {
+            e.preventDefault();
+            let path = 'profile/' + user.username;
+            navigate(path);
+        };
+
+        return (
+            <Fragment>
+                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                    <Tooltip title="Đăng nhập">
+                        <IconButton
+                            onClick={handleClickLoggedIn}
+                            size="small"
+                            sx={{ ml: 2 }}
+                            aria-controls={open ? 'account-menu' : undefined}
+                            aria-haspopup="true"
+                            aria-expanded={open ? 'true' : undefined}
+                        >
+                            <Avatar src={PF + user.avatar} sx={{ width: 40, height: 40 }}></Avatar>
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+                <Menu
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            overflow: 'visible',
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
                             },
-                        }}
-                        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                    >
-                        <MenuItem>
-                            <Avatar /> Profile
-                        </MenuItem>
-                        <MenuItem>
-                            <Avatar /> My account
-                        </MenuItem>
-                        <Divider />
-                        <MenuItem>
-                            <ListItemIcon>
-                                <PersonAdd fontSize="small" />
-                            </ListItemIcon>
-                            Add another account
-                        </MenuItem>
-                        <MenuItem>
-                            <ListItemIcon>
-                                <Settings fontSize="small" />
-                            </ListItemIcon>
-                            Settings
-                        </MenuItem>
-                        <MenuItem onClick={handleClickLogOut}>
-                            <ListItemIcon>
-                                <Logout fontSize="small" />
-                            </ListItemIcon>
-                            Logout
-                        </MenuItem>
-                    </Menu>
-                </Fragment>
-            ) : (
-                // Not logged in yet
-                <Fragment>
-                    <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                        <Tooltip title="Đăng nhập">
-                            <IconButton
-                                onClick={handleClickNoLoggedIn}
-                                size="small"
-                                sx={{ ml: 2 }}
-                                aria-controls={open ? 'account-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={open ? 'true' : undefined}
-                            >
-                                <Avatar sx={{ width: 40, height: 40 }} alt="avatar" src={PF + 'images/noUser.png'} />
-                            </IconButton>
-                        </Tooltip>
-                    </Box>
-                </Fragment>
-            )}
-        </Fragment>
-    );
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: 'background.paper',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
+                        },
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClickProfile}>
+                        <Avatar src={PF + user.avatar} /> Trang cá nhân
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem>
+                        <ListItemIcon>
+                            <PersonAdd fontSize="small" />
+                        </ListItemIcon>
+                        Thêm tài khoản
+                    </MenuItem>
+                    <MenuItem>
+                        <ListItemIcon>
+                            <Settings fontSize="small" />
+                        </ListItemIcon>
+                        Cài đặt
+                    </MenuItem>
+                    <MenuItem onClick={handleClickLogOut}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" />
+                        </ListItemIcon>
+                        Đăng xuất
+                    </MenuItem>
+                </Menu>
+            </Fragment>
+        );
+    };
+
+    const NoLogged = () => {
+        const handleClickNoLoggedIn = (e) => {
+            e.preventDefault();
+            if (isLoggedIn === null) {
+                dispatch(FormLogin());
+                dispatch(OpenModalLogin());
+            }
+        };
+
+        return (
+            <Fragment>
+                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                    <Tooltip title="Đăng nhập">
+                        <IconButton onClick={handleClickNoLoggedIn} size="small" sx={{ ml: 2 }} aria-haspopup="true">
+                            <Avatar sx={{ width: 40, height: 40 }} alt="avatar" src={PF + 'images/noUser.png'} />
+                        </IconButton>
+                    </Tooltip>
+                </Box>
+            </Fragment>
+        );
+    };
+
+    return <Fragment>{isLoggedIn ? <Logged /> : <NoLogged />}</Fragment>;
 };
 
 export default AccountUser;
