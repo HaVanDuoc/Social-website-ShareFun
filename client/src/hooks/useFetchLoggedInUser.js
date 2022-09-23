8479194675306f44320e215e45e0ebeca6ee2bb3
Hà Văn Được <havanduoc.dev@gmail.com>
2022-09-23 15:50:50 +0700

    Create - customize hooks useFetchLoggedInUser

---
+++ client/src/hooks/useFetchLoggedInUser.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+
+function useFetchLoggedInUser() {
+    const [user, setUser] = useState();
+
+    useEffect(() => {
+        const loggedInUser = localStorage.getItem('user');
+        if (loggedInUser) {
+            const foundUser = JSON.parse(loggedInUser);
+            setUser(foundUser);
+        }
+    }, []);
+
+    return user
+}
+
+export default useFetchLoggedInUser
